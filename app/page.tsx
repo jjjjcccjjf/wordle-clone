3e373998a1c1023b1829a810bb84376ef4b39eb1
Ro11ingButler <rollingkai1214@gmail.com>
2023-09-04 02:27:39 +0800

    Fix focus stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,120 @@
 "use client";
 
-import { forwardRef, useEffect, useRef } from "react";
-
+import { forwardRef, useState, useEffect, useRef } from "react";
+import { setCurrentRow, setPlayerGuesses } from "./redux/slices/wordleSlice";
 import "@fontsource/roboto";
+import { RootState } from "./redux/store";
+import { useSelector, useDispatch } from "react-redux";
 
 const Input = forwardRef(function Input(props, ref) {
+  const playerGuesses = useSelector(
+    (state: RootState) => state.wordle.playerGuesses
+  );
+  const currentRow = useSelector((state: RootState) => state.wordle.currentRow);
+
+  let localRef = useRef();
+
+  const [letter, setLetter] = useState("");
+  const dispatch = useDispatch();
   const { row, col, inputRefs } = props;
   const map = inputRefs.current;
+
+  function animatePopToggle(node, toggleOn = true) {
+    if (toggleOn) {
+      node.classList.add("border-white/40");
+      node.classList.add("animate-pop");
+      node.classList.remove("border-white/20");
+    } else {
+      node.classList.remove("border-white/40");
+      node.classList.remove("animate-pop");
+      node.classList.add("border-white/20");
+    }
+  }
+
+  const handleChange = (e) => {
+    const nextCell = map.get(`cell-${row}-${col + 1}`);
+    const thisCell = map.get(`cell-${row}-${col}`);
+    if (e.target.value.length > 0) {
+      animatePopToggle(thisCell);
+    } else {
+      animatePopToggle(thisCell, false);
+    }
+    setLetter(e.target.value);
+    if (nextCell) {
+      nextCell.focus();
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    const thisCell = map.get(`cell-${row}-${col}`);
+    const prevCell = map.get(`cell-${row}-${col - 1}`);
+
+    if (e.key === "Backspace" && thisCell.value === "" && prevCell) {
+      animatePopToggle(prevCell, false);
+      prevCell.focus();
+      prevCell.value = "";
+    } else if (e.key === "Enter") {
+      if (thisCell.value !== "" && col === 4) {
+        console.log("pressed enter but accepted");
+
+        let guessWord = "";
+        Array.from(
+          { length: 5 },
+          (_, index) =>
+            (guessWord += map.get(`cell-${currentRow}-${index}`).value)
+        );
+
+        dispatch(setPlayerGuesses(guessWord));
+        dispatch(setCurrentRow(currentRow + 1));
+        // console.log(guessWord);
+      } else {
+        console.log(playerGuesses);
+        console.log("pressed enter but rejected");
+      }
+    }
+  };
+
+  // useEffect(() => {
+  //   if (letter.length > 0) {
+  //     localRef.current.classList.add("border-white/40");
+  //     localRef.current.classList.add("animate-pop");
+  //     localRef.current.classList.remove("border-white/20");
+  //   } else if (letter.length === 0) {
+  //     localRef.current.classList.remove("border-white/40");
+  //     localRef.current.classList.remove("animate-pop");
+  //     localRef.current.classList.add("border-white/20");
+  //   }
+  // }, [letter]);
+
   return (
     <input
+      value={letter}
       maxLength={1}
       className="h-14 w-14 border-2 border-white/20 bg-transparent text-center font-[Roboto] text-3xl font-bold uppercase text-white outline-none transition-transform duration-200 caret-transparent"
       ref={(node) => {
         if (node) {
           map.set(`cell-${row}-${col}`, node);
+          localRef.current = node;
         } else {
           map.delete(`cell-${row}-${col}`);
         }
       }}
-      onChange={() => {
-        const nextCell = map.get(`cell-${row}-${col + 1}`);
-        if (nextCell) {
-          nextCell.focus();
-        }
-      }}
-      onKeyDown={(e) => {
-        const thisCell = map.get(`cell-${row}-${col}`);
-        const prevCell = map.get(`cell-${row}-${col - 1}`);
-        
-        if (e.key === "Backspace" && thisCell.value === "" && prevCell) {
-          prevCell.focus();
-          prevCell.value = "";
-        }
-      }}
+      onChange={(e) => handleChange(e)}
+      onKeyDown={(e) => handleKeyDown(e)}
     ></input>
   );
 });
 
+////////////////////////////
+////////////////////////////
+////////////////////////////
+////////////////////////////
+////////////////////////////
+////////////////////////////
+
 function RowInput(props) {
   const { inputRefs, row } = props;
+
   return (
     <>
       <div className="flex flex-row gap-1">
@@ -50,15 +126,29 @@ function RowInput(props) {
   );
 }
 
+////////////////////////////
+////////////////////////////
+////////////////////////////
+////////////////////////////
+////////////////////////////
+////////////////////////////
+
 export default function Home() {
   const inputRefs = useRef(new Map());
-  const activeRow = 0;
+  const currentRow = useSelector((state: RootState) => state.wordle.currentRow);
+  const map = inputRefs.current;
+
+  const generateRegexFocusOnEmpty = (row) => new RegExp(`^cell-${row}-[0-4]$`);
+  const generateRegexFocusOnLast = (row) => new RegExp(`^cell-${row}-4$`);
 
   const handleClick = () => {
-    const map = inputRefs.current;
+    const regexEmpty = generateRegexFocusOnEmpty(currentRow);
+    const regexLast = generateRegexFocusOnLast(currentRow)
+    console.log(regexEmpty, regexLast);
+    
     if (map) {
       for (const [key, node] of map.entries()) {
-        if (node && node.value.length === 0) {
+        if ((node && node.value.length === 0 && regexEmpty.test(key)) || (node && node.value.length === 1 && regexLast.test(key))) {
           node.focus();
           break; // Focus the first empty input and stop iterating
         }
@@ -72,7 +162,12 @@ export default function Home() {
     return () => {
       document.removeEventListener("click", handleClick);
     };
-  }, []);
+  }, [currentRow]);
+
+  useEffect(() => {
+    const firstRowNode = map.get(`cell-${currentRow}-0`);
+    firstRowNode.focus();
+  }, [currentRow]);
 
   return (
     <>
---
+++ app/redux/slices/wordleSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+type WordleState = {
+  playerGuesses: Array<string>;
+  currentRow: number;
+  testWord: string;
+};
+
+// Define the initial state using that type
+const initialState: WordleState = {
+  playerGuesses: [],
+  currentRow: 0,
+  testWord: "THROE",
+};
+
+export const wordleSlice = createSlice({
+  name: "wordle",
+  initialState,
+  reducers: {
+    setCurrentRow: (state, action: PayloadAction<number>) => {
+      state.currentRow = action.payload;
+    },
+    setPlayerGuesses: (state, action: PayloadAction<string>) => {
+      state.playerGuesses.push(action.payload);
+    },
+  },
+});
+
+export const { setCurrentRow, setPlayerGuesses } =
+  wordleSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+// export const selectCount = (state: RootState) => state.counter.value
+
+export default wordleSlice.reducer;
