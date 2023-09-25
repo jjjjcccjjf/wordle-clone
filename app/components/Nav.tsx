32e97eae54bbce899dcd91f8339b6203cba12138
Ro11ingButler <rollingkai1214@gmail.com>
2023-09-25 23:45:14 +0800

    Fix nav on smaller screens

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -8,12 +8,12 @@ type NavProps = {
 
 export default function Nav({ children }: NavProps) {
   return (
-    <nav className="h-[66px] fixed top-0 left-0 w-full grid items-center border-b border-b-[#3a3a3c] bg-[#121213]">
+    <nav className="md:h-[66px] h-[41px] fixed top-0 left-0 w-full grid items-center border-b border-b-[#3a3a3c] bg-[#121213]">
       <ul className="grid grid-flow-col grid-cols-3  items-center">
         <li className="px-8"></li>
-        <li className="place-self-center flex items-center justify-center flex-col ">
-          <p className="font-['Patua_One']  text-white text-4xl">Wordle</p>
-          <p className="text-white font-[Roboto] text-[0.70rem] tracking-wider">
+        <li className="md:place-self-center place-self-end flex items-center justify-center md:flex-col flex-row md:gap-0">
+          <p className="font-['Patua_One']  text-white md:text-4xl text-2xl">Wordle</p>
+          <p className="text-white font-[Roboto] text-[0.70rem] tracking-wider w-32 md:pl-0 pl-3">
             clone by endan
           </p>
         </li>
